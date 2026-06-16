41882676dfeb6ae787ecba53861358aa81fda38f
Lanna Check <lannacheck@gmail.com>
2026-06-15 22:47:52 -0400

    Adding an animation to my fav works

--- js/default.js
+++ js/default.js
@@ -1,6 +1,7 @@
 window.onload = function () {
-    // Stores the elements to be checkes
-    const elements = document.querySelectorAll('.about-text, .about-image');
+    // Stores the elements to be checked
+    const aboutElements = document.querySelectorAll('.about-text, .about-image');
+    const favWorksElements = document.querySelectorAll('.fav-works');
 
     // Checks if the elements can be seen on screen
     const observer = new IntersectionObserver((entries) => {
@@ -14,6 +15,8 @@ window.onload = function () {
         // If a certain amount of the element is seen show it
         threshold: 0.2 // 20% of the element is visible
     });
+
     // Calls for the elements to be checked
-    elements.forEach(el => observer.observe(el));
+    aboutElements.forEach(el => observer.observe(el));
+    favWorksElements.forEach(el => observer.observe(el));
 }
